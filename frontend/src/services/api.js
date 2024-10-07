b06da977c567dc3fd664bd578db751f95b906151
Tim <techwithtimm@gmail.com>
2024-10-07 18:16:34 +0400

    make frontend use custom backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000"; // Replace with your actual API URL
+const API_BASE_URL = import.meta.env.VITE_API_URL; // Replace with your actual API URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
