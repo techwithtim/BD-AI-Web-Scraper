bbc881a277b3a0b4b635ee2dad4bd3ffd9266a8d
Tim <techwithtimm@gmail.com>
2024-10-07 18:41:01 +0400

    fix navbar css import

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../css/Navbar.css";
+import "../css/NavBar.css";
 import logo from "../assets/logo.png";
 
 const Navbar = ({ credits, isLoggedIn, login, logout }) => {
